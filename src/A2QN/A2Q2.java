c203753d4f3c69638accde7fc688db1abf0cf2d5
daupb7975 <daupb7975@HRH-IC0027322.wrdsb.ca>
2018-02-20 11:35:53 -0500

    Assignment 2 done question 1

---
+++ src/A2QN/A2Q2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package A2QN;
+
+/**
+ *
+ * @author daupb7975
+ */
+public class A2Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
