524c784ca771358f9562d49bfa0d0cb11a2ddca1
daupb7975 <daupb7975@HRH-IC0027322.wrdsb.ca>
2018-02-21 11:09:55 -0500

    added notes to assignment one

--- src/A2QN/A2Q2.java
+++ src/A2QN/A2Q2.java
@@ -7,6 +7,8 @@ package A2QN;
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.RobotSE;
+import becker.robots.Thing;
+import becker.robots.Wall;
 
 /**
  *
@@ -19,11 +21,47 @@ public class A2Q2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         //create city
-    City kw = new City();
-    
-    //spawn robot
-    RobotSE clarence = new RobotSE(kw, 1, 1, Direction.EAST);
+        City kw = new City();
+
+        //spawn robot
+        RobotSE clarence = new RobotSE(kw, 1, 0, Direction.EAST);
+
+        //create race track and hurdles
+        new Wall(kw, 1, 0, Direction.SOUTH);
+        new Wall(kw, 1, 1, Direction.SOUTH);
+        new Wall(kw, 1, 2, Direction.SOUTH);
+        new Wall(kw, 1, 3, Direction.SOUTH);
+        new Wall(kw, 1, 4, Direction.SOUTH);
+        new Wall(kw, 1, 5, Direction.SOUTH);
+        new Wall(kw, 1, 6, Direction.SOUTH);
+        new Wall(kw, 1, 7, Direction.SOUTH);
+        new Wall(kw, 1, 8, Direction.SOUTH);
+        new Wall(kw, 1, 1, Direction.WEST);
+        new Wall(kw, 1, 2, Direction.WEST);
+        new Wall(kw, 1, 4, Direction.WEST);
+        new Wall(kw, 1, 7, Direction.WEST);
+
+        //create thing
+        new Thing(kw, 1, 8);
+        
+        //loop that makes clarence run race and stop on thing
+        while (!clarence.canPickThing()) {
+            if (!clarence.frontIsClear()) {
+                clarence.turnLeft();
+                clarence.move();
+                clarence.turnRight();
+                clarence.move();
+                clarence.turnRight();
+                clarence.move();
+                clarence.turnLeft();
+
+            } else {
+                clarence.move();
+            }
+
+        }
+
     }
 }
