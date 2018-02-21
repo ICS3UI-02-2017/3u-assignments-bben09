781b59aba2306ec0f9770b5f2d68f9200689302d
daupb7975 <daupb7975@HRH-IC0027322.wrdsb.ca>
2018-02-21 10:36:24 -0500

    added notes to assignment one

--- src/A2QN/A2Q1.java
+++ src/A2QN/A2Q1.java
@@ -21,27 +21,33 @@ public class A2Q1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    City kw = new City();
-    RobotSE clarence = new RobotSE(kw, 1, 1, Direction.EAST);
-    new Thing(kw, 1, 2);
-    new Thing(kw, 1, 3);
-    new Thing(kw, 1, 4);
-    new Thing(kw, 1, 5);
-    new Thing(kw, 1, 6);
-    new Thing(kw, 1, 7);
-    new Thing(kw, 1, 8);
-    new Thing(kw, 1, 9);
-    new Thing(kw, 1, 10);
-    new Thing(kw, 1, 11);
-    
-    
-    while(clarence.getAvenue() != 12){
-        clarence.move();
-        if(clarence.canPickThing()){
-            if(clarence.countThingsInBackpack()  < 7){
-                clarence.pickThing();
+
+        //create city
+        City kw = new City();
+
+        //spawn robot
+        RobotSE clarence = new RobotSE(kw, 1, 1, Direction.EAST);
+
+        //create line of things
+        new Thing(kw, 1, 2);
+        new Thing(kw, 1, 3);
+        new Thing(kw, 1, 4);
+        new Thing(kw, 1, 5);
+        new Thing(kw, 1, 6);
+        new Thing(kw, 1, 7);
+        new Thing(kw, 1, 8);
+        new Thing(kw, 1, 9);
+        new Thing(kw, 1, 10);
+        new Thing(kw, 1, 11);
+
+        //move clarence along line of thing picking up 7 and stopping at (1,12)
+        while (clarence.getAvenue() != 12) {
+            clarence.move();
+            if (clarence.canPickThing()) {
+                if (clarence.countThingsInBackpack() < 7) {
+                    clarence.pickThing();
+                }
             }
         }
-    }
     }
 }
