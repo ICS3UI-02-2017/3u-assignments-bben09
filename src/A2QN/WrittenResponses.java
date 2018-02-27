14e0b363b8b87f60e10af7394704e01e65dc6595
daupb7975 <daupb7975@HRH-IC0027322.wrdsb.ca>
2018-02-27 10:29:40 -0500

    Finished A2 written responses

---
+++ src/A2QN/WrittenResponses.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package A2QN;
+
+/**
+ *
+ * @author daupb7975
+ */
+public class WrittenResponses {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
+
+1. a. false
+   b. true
+   c. false
+   d. false
+   e. false
+   f. true
+   g. false
+
+2. they behave the same, they both say if karel can pick thing turn right, and if she cant turn left
