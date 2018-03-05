e5346719fb3f2e99512b6dc2ddd485e1865b1997
daupb7975 <daupb7975@HRH-IC0027322.wrdsb.ca>
2018-03-05 10:23:38 -0500

    Started assignment 4

---
+++ src/A4Qn/A4Q1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package A4Qn;
+
+/**
+ *
+ * @author daupb7975
+ */
+public class A4Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //
+        System.out.println("Enter your name:");
+        
+    }
+}
