54b907cba03009558ed3bad775f6f2e73cdac9ef
daupb7975 <daupb7975@HRH-IC0027322.wrdsb.ca>
2018-03-06 10:38:13 -0500

    Finished A4Q3

--- src/A4Qn/A4Q3.java
+++ src/A4Qn/A4Q3.java
@@ -4,6 +4,8 @@
  */
 package A4Qn;
 
+import java.util.Scanner;
+
 /**
  *
  * @author daupb7975
@@ -15,5 +17,25 @@ public class A4Q3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //text to tell user to input 4 numbers
+        System.out.println("Please enter 4 numbers");
+        
+        //scanner to read numbers
+        Scanner input = new Scanner (System.in);
+        
+        //floats to allow user to input the numbers, and read them later
+        float number1 = input.nextFloat();
+        
+        float number2 = input.nextFloat();
+        
+        float number3 = input.nextFloat();
+        
+        float number4 = input.nextFloat();
+        
+        //text listing the users inputed numbers
+        System.out.println("Your numbers were: " + number1 + ", " + number2 + ", " + number3 + ", and " + number4);
+                
+                
     }
 }
