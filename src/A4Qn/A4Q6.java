c0e9f5a33dbaa78f4f7ce8d84692687eb94b7035
daupb7975 <daupb7975@HRH-IC0027322.wrdsb.ca>
2018-03-20 10:56:57 -0400

    Finished a4q6

--- src/A4Qn/A4Q6.java
+++ src/A4Qn/A4Q6.java
@@ -4,6 +4,8 @@
  */
 package A4Qn;
 
+import java.util.Scanner;
+
 /**
  *
  * @author daupb7975
@@ -15,5 +17,40 @@ public class A4Q6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+       
+        //Scanner
+        Scanner input = new Scanner(System.in);
+        
+        //Tell user to input speed and speed limit and make them integers
+        System.out.println("Please enter the speed limit:");
+        int limit = input.nextInt();
+        System.out.println("Please enter your speed:");
+        int speed = input.nextInt();
+        
+        //make an integer to see how much over the limit they are
+        int fine = (speed - limit);
+
+       
+           //if loops to register the speed over the limit and tell them what the fine is
+            if(fine < 0) {
+                System.out.println("Congradulations you are within the speed limit.");
+            }
+              if(fine > 0 && fine <21){
+                System.out.println("You are speeding your fine is $100.");
+            }
+
+             if (fine >= 21 && fine < 31) {
+                System.out.println("You are speeding and your fine is $270.");
+
+            }
+             if (fine >= 31) {
+                System.out.println("You are speeding and your fine is $500.");
+            }
+           
+              
+
+
+        
+
+    
 }
