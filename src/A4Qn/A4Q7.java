23d9028e5c8a404e293f33f3883f4eb75bb9c026
daupb7975 <daupb7975@HRH-IC0027322.wrdsb.ca>
2018-03-21 10:32:52 -0400

    Finished A4Q7

--- src/A4Qn/A4Q7.java
+++ src/A4Qn/A4Q7.java
@@ -17,41 +17,52 @@ public class A4Q7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         //scanner
-        Scanner input = new Scanner (System.in);
-        
-        int blank = 0;
+        Scanner input = new Scanner(System.in);
+
         int posi = 1;
-        
-        while(blank == 0){
-        
-        System.out.println("Enter your dice number:");
-        int roll = input.nextInt();
-        
-        posi = (posi + roll);
-        
-        if(posi == 54){
-            posi = 19;
-        }
-        if(posi == 90){
-            posi = 48;
-        }
-        if(posi == 99){
-            posi = 77;
-        }
-        if(posi == 9){
-            posi = 34;
-        }
-        if(posi == 40){
-            posi = 64;
-        }
-        if(posi == 67){
-            posi = 86;
-        }
+
+        while (posi != 100) {
+
+            System.out.println("Enter your dice number:");
+            int roll = input.nextInt();
+
+            if (roll < 2) {
+                System.out.println("Not valid roll.");
+            }
+            if (roll > 12) {
+                roll = 0;
+                System.out.println("Not valid roll.");
+            }
+            if (posi > 100) {
+                roll = 0;
+            }
+            posi = (posi + roll);
+
+            if (posi == 54) {
+                posi = 19;
+            }
+            if (posi == 90) {
+                posi = 48;
+            }
+            if (posi == 99) {
+                posi = 77;
+            }
+            if (posi == 9) {
+                posi = 34;
+            }
+            if (posi == 40) {
+                posi = 64;
+            }
+            if (posi == 67) {
+                posi = 86;
+            }
             System.out.println("Your position is " + posi);
-        
+
         }
-        
+        if (posi == 100) {
+            System.out.println("Congradulation, you won!");
+        }
     }
 }
