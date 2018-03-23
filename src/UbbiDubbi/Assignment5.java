5e835f1663729d5f5394e741a1db47cb5dc7e531
daupb7975 <daupb7975@HRH-IC0027322.wrdsb.ca>
2018-03-23 11:18:23 -0400

    Finished A4Q10

--- src/UbbiDubbi/Assignment5.java
+++ src/UbbiDubbi/Assignment5.java
@@ -17,42 +17,49 @@ public class Assignment5 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        Scanner input = new Scanner (System.in);
-        
-        while(true){
-        System.out.println("Enter word you would like to translate:");
-        
-        String Word = input.nextLine();
-        
-        Word = Word.toLowerCase();
-        
-                    if(Word.startsWith("a") || 
-                    Word.startsWith("e") || 
-                    Word.startsWith("i") || 
-                    Word.startsWith("o") ||
-                    Word.startsWith("u")){
-        
-                //stick ay on the end
-            String transWord = Word + " is ub" + Word + " in Ubbi Dubbi";
-            System.out.println(transWord);
-            }else{
-                        int length = origWord.length();
-                    
-                
-                //use a for loop to go through the characters
-                for(int i = 0; i < length; i++){
-                    
-                    //look at the character, at position i, is it a vowel?
-                    if(Word.charAt(i) == 'a' || 
-                            Word.charAt(i) == 'e' || 
-                            Word.charAt(i) == 'i' ||
-                            Word.charAt(i) == 'o' ||
-                            Word.charAt(i) == 'u' ){
-                        String start = Word.substring(i)
-                    }
-        
-                    }
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter word you would like to translate:");
+
+            String Word = input.nextLine();
+
+            Word = Word.toLowerCase();
+
+
+            int length = Word.length();
+
+            String newWord = "";
+            //use a for loop to go through the characters
+            for (int i = 0; i < length; i++) {
+
+                //look at the character, at position i, is it a vowel?
+                if (Word.charAt(i) == 'a'
+                        || Word.charAt(i) == 'e'
+                        || Word.charAt(i) == 'i'
+                        || Word.charAt(i) == 'o'
+                        || Word.charAt(i) == 'u') {
+                    newWord = newWord + "ub" + Word.charAt(i);
+                } else {
+                    newWord = newWord + Word.charAt(i);
                 }
+
+                if (Word.charAt(i) == 'a'
+                        || Word.charAt(i) == 'e'
+                        || Word.charAt(i) == 'i'
+                        || Word.charAt(i) == 'o'
+                        || Word.charAt(i) == 'u'
+                        || Word.charAt(i + 1) == 'a'
+                        || Word.charAt(i + 1) == 'e'
+                        || Word.charAt(i + 1) == 'i'
+                        || Word.charAt(i + 1) == 'o'
+                        || Word.charAt(i + 1) == 'u'){
+                    newWord = newWord + Word.charAt(i);
+                    
+                }
+            }
+            System.out.println(Word + " is " + newWord + " in Ubbi Dubbi");
+        }
     }
 }
