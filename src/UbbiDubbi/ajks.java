93e61af85c9c5f89f8718e72daae5241943e9df0
daupb7975 <daupb7975@HRH-IC0027322.wrdsb.ca>
2018-04-03 11:20:02 -0400

    Almost done translator

--- src/UbbiDubbi/ajks.java
+++ src/UbbiDubbi/ajks.java
@@ -10,14 +10,13 @@ import java.util.Scanner;
  *
  * @author daupb7975
  */
-public class Assignment5 {
+public class ajks {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-
         Scanner input = new Scanner(System.in);
 
         while (true) {
@@ -43,55 +42,47 @@ public class Assignment5 {
                         && Word.charAt(i) != 'u') {
                     newWord = newWord + Word.charAt(i);
                 }
-
                 if (Word.charAt(i) == 'a'
-                        || Word.charAt(i) == 'e'
-                        || Word.charAt(i) == 'i'
-                        || Word.charAt(i) == 'o'
-                        || Word.charAt(i) == 'u') {
-
+                        && Word.charAt(i) == 'e'
+                        && Word.charAt(i) == 'i'
+                        && Word.charAt(i) == 'o'
+                        && Word.charAt(i) == 'u') {
                     position = i;
                 }
 
-                
-                //look at the character, at position i, is it a vowel?
-                if ((position != length - 1) && (position != 0) && (Word.charAt(i) == 'a'
+                if ((position != 0) && (Word.charAt(i) == 'a'
                         || Word.charAt(i) == 'e'
                         || Word.charAt(i) == 'i'
                         || Word.charAt(i) == 'o'
                         || Word.charAt(i) == 'u')
-                        && Word.charAt(position + 1) != 'a'
-                        && Word.charAt(position + 1) != 'e'
-                        && Word.charAt(position + 1) != 'i'
-                        && Word.charAt(position + 1) != 'o'
-                        && Word.charAt(position + 1) != 'u'
-                        && Word.charAt(position - 1) != 'a'
-                        && Word.charAt(position - 1) != 'e'
-                        && Word.charAt(position - 1) != 'i'
-                        && Word.charAt(position - 1) != 'o'
-                        && Word.charAt(position - 1) != 'u') {
+                        && Word.charAt(i - 1) != 'a'
+                        && Word.charAt(i - 1) != 'e'
+                        && Word.charAt(i - 1) != 'i'
+                        && Word.charAt(i - 1) != 'o'
+                        && Word.charAt(i - 1) != 'u') {
                     newWord = newWord + "ub" + Word.charAt(i);
                 }
-
-                if ((position != length - 1) && (Word.charAt(i) == 'a'
+                if ((position != 0) && (Word.charAt(i) == 'a'
                         || Word.charAt(i) == 'e'
                         || Word.charAt(i) == 'i'
                         || Word.charAt(i) == 'o'
                         || Word.charAt(i) == 'u')
-                        && (Word.charAt(position + 1) == 'a'
-                        || Word.charAt(position + 1) == 'e'
-                        || Word.charAt(position + 1) == 'i'
-                        || Word.charAt(position + 1) == 'o'
-                        || Word.charAt(position + 1) == 'u')) {
-                    newWord = newWord + "ub" + Word.charAt(i) + Word.charAt(position + 1);
+                        && (Word.charAt(i - 1) == 'a'
+                        || Word.charAt(i - 1) == 'e'
+                        || Word.charAt(i - 1) == 'i'
+                        || Word.charAt(i - 1) == 'o'
+                        || Word.charAt(i - 1) == 'u')) {
+                    newWord = newWord + Word.charAt(i);
                 }
-                
 
 
-
-
+                if ((position == 0) && (Word.charAt(i) == 'a'
+                        && Word.charAt(i) == 'e'
+                        && Word.charAt(i) == 'i'
+                        && Word.charAt(i) == 'o'
+                        && Word.charAt(i) == 'u')) {
+                    newWord = newWord + "ub" + Word.charAt(i);
+                }
             }
-            System.out.println(Word + " is " + newWord + " in Ubbi Dubbi");
+            System.out.println(Word + " is " + newWord + " in Ubbi Dubbi.");
         }
-    }
-}
