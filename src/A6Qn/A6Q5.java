4bfaa2ac26fa9ca32b96951140c0fddfdfb993a9
daupb7975 <daupb7975@HRH-IC0027322.wrdsb.ca>
2018-04-09 11:13:48 -0400

    Added comments to A6Q1,2,3,4, and finished A6Q6

--- src/A6Qn/A6Q5.java
+++ src/A6Qn/A6Q5.java
@@ -18,23 +18,23 @@ public class A6Q5 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
-        
+
         System.out.println("How many marks would you like to input?");
-        
+
         int numMarks = input.nextInt();
-        
+
         double[] marks = new double[numMarks];
-        
+
         double blank = 0;
-                
+
         System.out.println("Input the " + numMarks + " marks.");
         for (int i = 0; i < marks.length; i++) {
             marks[i] = input.nextDouble();
         }
         for (int i = 0; i < marks.length - 1; i++) {
             for (int j = i + 1; j < marks.length; j++) {
-                
-                if(marks[i] > marks[j]){
+
+                if (marks[i] > marks[j]) {
                     blank = marks[i];
                     marks[i] = marks[j];
                     marks[j] = blank;
@@ -45,8 +45,14 @@ public class A6Q5 {
         for (int i = 0; i < marks.length; i++) {
             System.out.println(marks[i] + ",");
         }
-            int half = marks.length / 2;
-            System.out.println("The median mark is " + marks[half]);
-        
+
+        if ((numMarks % 2) == 0) {
+            int posi = numMarks / 2;
+            
+        }else{
+            double middle1 = numMarks / 2;
+            double middle2 = Math.ciel
+            System.out.println("");
+        }
     }
 }
