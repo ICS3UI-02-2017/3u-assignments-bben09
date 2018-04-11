ca71e6a3beefab25daf7f901151d54b0c55f0911
daupb7975 <daupb7975@HRH-IC0027322.wrdsb.ca>
2018-04-11 10:48:17 -0400

    added comments to all questions in a6 besides 5

--- src/A6Qn/A6Q4.java
+++ src/A6Qn/A6Q4.java
@@ -30,10 +30,11 @@ public class A6Q4 {
         //make a blank double to store the value later
         double blank = 0;
 
-        //for loop to read the 
+        //for loop to read the marks and store them in the array
         for (int i = 0; i < marks.length; i++) {
             marks[i] = input.nextDouble();
         }
+        //bubble sorting for loop that puts the array in order from smallest to largest
         for (int i = 0; i < marks.length - 1; i++) {
             for (int j = i + 1; j < marks.length; j++) {
 
@@ -44,6 +45,7 @@ public class A6Q4 {
                 }
             }
         }
+        //output the marks in order from lowest the highest
         System.out.println(marks[0] + ", " + marks[1] + ", " + marks[2] + ", " + marks[3] + ", " + marks[4] + ", " + marks[5] + ", " + marks[6] + ", " + marks[7] + ", " + marks[8] + ", " + marks[9]);
     }
 }
--- src/A6Qn/A6Q6.java
+++ src/A6Qn/A6Q6.java
@@ -17,24 +17,34 @@ public class A6Q6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //scanner to read users input
         Scanner input = new Scanner(System.in);
 
+        //ask user how many students are in the class
         System.out.println("How many students are in the class?");
 
+        //store the class size in an integer
         int classsize = input.nextInt();
 
+        //make a double array the size of the class
         double[] marks = new double[classsize];
 
+        //create a blank double to use later
         double blank = 0;
 
+        //create a blank double to use later
         double total = 0;
 
+        //ask user to input the marks of their students
         System.out.println("Input the marks of the " + classsize + " students now.");
 
+        //store the marks in the array
         for (int i = 0; i < marks.length; i++) {
             marks[i] = input.nextDouble();
         }
 
+        //bubble sorting for loop to put the marks in order from smallest to biggest
         for (int i = 0; i < marks.length - 1; i++) {
             for (int j = i + 1; j < marks.length; j++) {
 
@@ -46,11 +56,14 @@ public class A6Q6 {
             }
         }
 
+        //for loop to total the marks in the class
         for (int i = 0; i < marks.length; i++) {
             total = total + marks[i];
         }
+        //get the average mark by dividing the total by the length of the array (the number of marks in the class)
         double avg = total / marks.length;
         
+        //output the lowest mark, the highest mark, and the average
         System.out.println("The lowest mark is " + marks[0] + ", the highest mark is " + marks[marks.length - 1] + ", the class average is " + avg + ".");
     }
 }
