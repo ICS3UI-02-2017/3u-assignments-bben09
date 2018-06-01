7d822161f910a187bcefd96ad73b30aa641932eb
daupb7975 <daupb7975@HRH-IC0027322.wrdsb.ca>
2018-06-01 11:20:19 -0400

    Got rng and collision detection

--- src/Game.java
+++ src/Game.java
@@ -35,21 +35,31 @@ public class Game extends JComponent implements ActionListener {
     // this is what keeps our time running smoothly :)
     Timer gameTimer;
     // YOUR GAME VARIABLES WOULD GO HERE
+    //booleans for the controls
     boolean up = false;
     boolean down = false;
     boolean right = false;
     boolean left = false;
+    //boolean to start the game
     boolean start = false;
+    boolean lvl1 = false;
+    boolean lvl2 = false;
+    boolean lvl2start = false;
+    //boolean to end the game
     boolean gameover = false;
+    //integers to keep track of the players position
     int playerX = 375;
     int playerY = 550;
-    int Y1 = 10;
-    int Y2 = 215;
-    int Y3 = 415;
-    int Y4 = 615;
-    int[] randomwidth = new int[100];
-    int[] rightrectx = new int[100];
-    int[] yvalues = new int[100];
+    //integer array to store the width of the left side walls
+    int[] randomwidthlvl1 = new int[10];
+    int[] randomwidthlvl2 = new int[10];
+    //integer array to store the x value of the right side walls
+    int[] rightrectxlvl1 = new int[10];
+    int[] rightrectxlvl2 = new int[10];
+    //integer array to store the y values for the walls
+    int[] yvalueslvl1 = new int[10];
+    int[] yvalueslvl2 = new int[10];
+    //font for outputing to user
     Font gametext = new Font("arial", Font.BOLD, 50);
 
     // GAME VARIABLES END HERE    
@@ -92,28 +102,45 @@ public class Game extends JComponent implements ActionListener {
         g.clearRect(0, 0, WIDTH, HEIGHT);
 
         // GAME DRAWING GOES HERE
+
+        //make a black background
         g.setColor(Color.black);
         g.fillRect(0, 0, 800, 600);
 
+        //draw a yellow square, this is the player
         g.setColor(Color.yellow);
         g.fillRect(playerX, playerY, 40, 40);
 
         g.setColor(Color.white);
-        //g.fillRect(0, Y1, 300, 50);
-        //g.fillRect(375, Y1, 425, 50);
 
-        //Rectangle firstwallR = new Rectangle(375, Y1, 425, 50);
-
-        for (int i = 0; i < 100; i++) {
-            g.fillRect(0, yvalues[i], randomwidth[i], 50);
-            g.fillRect(rightrectx[i], yvalues[i], 800, 50);
+        //draw all the walls for level one above the screen before they move down towards the player
+        if (lvl1) {
+            for (int i = 0; i < 10; i++) {
+                g.fillRect(0, yvalueslvl1[i], randomwidthlvl1[i], 50);
+                g.fillRect(rightrectxlvl1[i], yvalueslvl1[i], 800, 50);
+            }
         }
 
-        if (!start && !gameover) {
+        //draw all the walls for level two above the screen before they move down towards the player
+        if (lvl2) {
+            for (int i = 0; i < 10; i++) {
+                g.fillRect(0, yvalueslvl2[i], randomwidthlvl2[i], 50);
+                g.fillRect(rightrectxlvl2[i], yvalueslvl2[i], 800, 50);
+            }
+        }
+        //tell player to press space to start level one
+        if (!lvl1 && !gameover && !lvl2start) {
             g.setFont(gametext);
             g.setColor(Color.orange);
-            g.drawString("Press Space to Start Game", 100, 100);
+            g.drawString("Press Space to Start Level One", 50, 100);
         }
+        //tell the player to press space to start level two
+        if (lvl2start && !gameover) {
+            g.setFont(gametext);
+            g.setColor(Color.orange);
+            g.drawString("Press Space to Start Level Two", 50, 100);
+        }
+        //if the game is over, output "Game Over"
         if (gameover) {
             g.setFont(gametext);
             g.setColor(Color.orange);
@@ -127,57 +154,107 @@ public class Game extends JComponent implements ActionListener {
     // This is run before the game loop begins!
     public void preSetup() {
         // Any of your pre setup before the loop starts should go here
-        for (int i = 0; i < 100; i++) {
-            randomwidth[i] = (int) Math.floor(Math.random() * 725);
-            rightrectx[i] = randomwidth[i] + 75;
+
+        //for level one: randomly generate the width of the wall on the left side, then make the x on the left equal to the width plus 80 to make and opening for the player
+        for (int i = 0; i < 10; i++) {
+            randomwidthlvl1[i] = (int) Math.floor(Math.random() * 725);
+            rightrectxlvl1[i] = randomwidthlvl1[i] + 80;
         }
 
-        for (int i = 0; i < 100; i++) {
-            yvalues[i] = (i * 200) * -1;
-
+        //for level one: fill y value array for the walls that will seperate them by 200
+        for (int i = 0; i < 10; i++) {
+            yvalueslvl1[i] = ((i * 200) * -1) - 50;
         }
+        // for level 2: randomly generate the width of the wall on the left side, then make the x on the left equal to the width plus 80 to make and opening for the player
+        for (int i = 0; i < 10; i++) {
+            randomwidthlvl2[i] = (int) Math.floor(Math.random() * 725);
+            rightrectxlvl2[i] = randomwidthlvl2[i] + 80;
+        }
+
+        //for level 2: fill y value array for the walls that will seperate them by 200
+        for (int i = 0; i < 10; i++) {
+            yvalueslvl2[i] = ((i * 200) * -1) - 50;
+        }
+
     }
 
     // The main game loop
     // In here is where all the logic for my game will go
     public void gameLoop() {
+
+        //make player move up but dont let them pass the top of the screen
         if (up && playerY > -1 && start) {
             playerY = playerY - 8;
         }
+        //make player move down but dont let them pass the bottom of the screen
         if (down && playerY < 551 && start) {
             playerY = playerY + 8;
         }
+        //make player move right but dont let them go off the right of the screen
         if (right && playerX < 751 && start) {
             playerX = playerX + 8;
         }
+        //make player move left but dont let them go off the left side of the screen
         if (left && playerX > -1 && start) {
             playerX = playerX - 8;
         }
 
-        if (start) {
-            for (int i = 0; i < 100; i++) {
-                yvalues[i] = yvalues[i] + 4;
+        //once the player starts the game, make the walls move down towards the player
+        if (lvl1) {
+            for (int i = 0; i < 10; i++) {
+                yvalueslvl1[i] = yvalueslvl1[i] + 2;
             }
-            for (int i = 0; i < 100; i++) {
-                if (!(playerX > randomwidth[i] || playerY + 40 < yvalues[i] || playerY > yvalues[i] + 50)) {
+
+            //if the player touches one of the walls on the left side, stop the game and display gameover
+            for (int i = 0; i < 10; i++) {
+                if (!(playerX > randomwidthlvl1[i] || playerY + 40 < yvalueslvl1[i] || playerY > yvalueslvl1[i] + 50)) {
                     gameover = true;
                 }
             }
-            for (int i = 0; i < 100; i++) {
-                if (!(playerX + 40 < rightrectx[i] || playerY + 40 < yvalues[i] || playerY > yvalues[i] + 50)) {
+            //if the player touches one of the walls on the right side, stop the game and display gameover
+            for (int i = 0; i < 10; i++) {
+                if (!(playerX + 40 < rightrectxlvl1[i] || playerY + 40 < yvalueslvl1[i] || playerY > yvalueslvl1[i] + 50)) {
                     gameover = true;
                 }
             }
 
+            //if the game is over, make stop the game from running
             if (gameover) {
+                lvl1 = false;
                 start = false;
-                up = false;
-                down = false;
-                right = false;
-                left = false;
             }
         }
-        
+        if (yvalueslvl1[9] == 600) {
+            lvl1 = false;
+            lvl2start = true;
+        }
+        if (lvl2) {
+            for (int i = 0; i < 10; i++) {
+                yvalueslvl2[i] = yvalueslvl2[i] + 3;
+            }
+
+            //if the player touches one of the walls on the left side, stop the game and display gameover
+            for (int i = 0; i < 10; i++) {
+                if (!(playerX > randomwidthlvl2[i] || playerY + 40 < yvalueslvl2[i] || playerY > yvalueslvl2[i] + 50)) {
+                    gameover = true;
+                }
+            }
+            //if the player touches one of the walls on the right side, stop the game and display gameover
+            for (int i = 0; i < 10; i++) {
+                if (!(playerX + 40 < rightrectxlvl2[i] || playerY + 40 < yvalueslvl2[i] || playerY > yvalueslvl2[i] + 50)) {
+                    gameover = true;
+                }
+            }
+
+            //if the game is over, make stop the game from running
+            if (gameover) {
+                start = false;
+                lvl2 = false;
+            }
+        }
+        if (yvalueslvl2[9] == 600) {
+            lvl2 = false;
+        }
 
     }
 
@@ -223,8 +300,14 @@ public class Game extends JComponent implements ActionListener {
                 right = true;
             }
             if (keycode == KeyEvent.VK_SPACE) {
+                lvl1 = true;
                 start = true;
             }
+            if (lvl2start && keycode == KeyEvent.VK_SPACE) {
+                lvl2 = true;
+                lvl2start = false;
+            }
+            
         }
 
         // if a key has been released
